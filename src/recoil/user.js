a1fa6ca66e8784ec948314aadfe152c449b96697
zhangzhiguo <1530006432@qq.com>
2022-07-29 14:03:14 +0800

    feat: init

--- src/recoil/user.js
+++ src/recoil/user.js
@@ -4,3 +4,5 @@ export const textState = atom({
     key: 'textState', // unique ID (with respect to other atoms/selectors)
     default: 'cclr', // default value (aka initial value)
 });
+
+console.log( textState);
